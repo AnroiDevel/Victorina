2386f57b3b93b8cf530b351d8a0db0d8b40a803e
AnroiDevel <AnroiDevel@gmail.com>
2022-02-13 03:25:10 +0300

    rezerv

--- Assets/Resources/Thema.cs
+++ Assets/Resources/Thema.cs
@@ -13,5 +13,18 @@ namespace Victorina
         public Sprite GameProgressPanel;
         public Sprite GamePanel;
         public Sprite GameExitPanel;
+
+        public Sprite MainMenuBack;
+        public Sprite ShopMenuBack;
+        public Sprite StatisticsPanelBack;
+        public Sprite AchivPanelBack;
+
+        [Header("Меню")]
+        public Sprite BottomAnimMenuBack;
+        public Sprite OptionIcon;
+        public Sprite ReitingIcon;
+        public Sprite MainIcon;
+        public Sprite ShopIcon;
+        public Sprite OptionMenuBack;
     }
 }
---
+++ Assets/Scripts/MainMenu/MainMenuThema.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Victorina
+{
+    public class MainMenuThema : MonoBehaviour
+    {
+        [SerializeField] private Image _backgroundMainMenu;
+        [SerializeField] private Image _backgroundShopMenu;
+        [SerializeField] private Image _backgroundOptionMenu;
+        [SerializeField] private Image _statisticsPanel;
+        [SerializeField] private Image _achivPanel;
+
+
+
+        [Header("Меню")]
+        [SerializeField] private Image _bottomAnimMenuBack;
+        [SerializeField] private Image _optionIcon;
+        [SerializeField] private Image _reitingIcon;
+        [SerializeField] private Image _mainIcon;
+        [SerializeField] private Image _shopIcon;
+
+
+        private void Start()
+        {
+            _backgroundMainMenu.sprite = ThemaConrtoller.ActiveThema?.MainMenuBack;
+            _backgroundShopMenu.sprite = ThemaConrtoller.ActiveThema?.ShopMenuBack;
+            _backgroundOptionMenu.sprite = ThemaConrtoller.ActiveThema?.OptionMenuBack;
+            _statisticsPanel.sprite = ThemaConrtoller.ActiveThema?.StatisticsPanelBack;
+            _achivPanel.sprite = ThemaConrtoller.ActiveThema?.AchivPanelBack;
+
+
+            _bottomAnimMenuBack.sprite = ThemaConrtoller.ActiveThema?.BottomAnimMenuBack;
+            _optionIcon.sprite = ThemaConrtoller.ActiveThema?.OptionIcon;
+            _reitingIcon.sprite = ThemaConrtoller.ActiveThema?.ReitingIcon;
+            _mainIcon.sprite = ThemaConrtoller.ActiveThema?.MainIcon;
+            _shopIcon.sprite = ThemaConrtoller.ActiveThema?.ShopIcon;
+
+        }
+    }
+}
